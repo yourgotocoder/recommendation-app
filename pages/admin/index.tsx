d9d03cad569c2234412f579570a570d8ca379222
yourgotocoder <yourgotocoder@gmail.com>
2022-06-06 12:06:19 +0530

    Adding handler for draggin file

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,18 +1,44 @@
 import type { NextPage } from "next";
-import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { ChangeEventHandler, InputHTMLAttributes, useContext } from "react";
+import { LegacyRef, RefObject, useContext, useEffect, useRef } from "react";
 import AuthContext from "../../store/AuthContext";
 import * as xlsx from "xlsx";
+import Button from "@mui/material/Button";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 
 const AdminPage: NextPage = () => {
     const authCtx = useContext(AuthContext);
     const router = useRouter();
     console.log(authCtx, "From admin");
+
     if (authCtx.status == "unauthenticated") {
         router.replace("/");
     }
+
+    const DnDAreaRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        DnDAreaRef.current?.addEventListener("dragover", handleDragOver);
+        DnDAreaRef.current?.addEventListener("drop", handleDrop);
+
+        return () => {
+            DnDAreaRef.current?.removeEventListener("dragover", handleDragOver);
+            DnDAreaRef.current?.removeEventListener("drop", handleDrop);
+        };
+    }, []);
+
+    const handleDragOver = (e: DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(e.dataTransfer?.types);
+    };
+    const handleDrop = (e: DragEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        const { files } = e.dataTransfer!;
+        console.log(files);
+    };
 
     const readUploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
@@ -46,14 +72,41 @@ const AdminPage: NextPage = () => {
             </Head>
 
             <main>
-                <div>
-                    Upload
-                    <input
-                        type="file"
-                        name="upload"
-                        id="upload"
-                        onChange={readUploadFile}
-                    />
+                <div
+                    style={{
+                        display: "flex",
+                        margin: "auto",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column",
+                        width: 400,
+                        flexWrap: "wrap",
+                        border: "1px dotted black",
+                        padding: "1rem",
+                    }}
+                    ref={DnDAreaRef}
+                >
+                    <div>
+                        <p>Drag and drop file here</p>
+                    </div>
+                    <div>
+                        <input
+                            type="file"
+                            accept="xlsx"
+                            style={{ display: "none" }}
+                            onChange={readUploadFile}
+                            id="contained-button-file"
+                        />
+                        <label htmlFor="contained-button-file">
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                component="span"
+                            >
+                                <UploadFileIcon color="action" />
+                            </Button>
+                        </label>
+                    </div>
                 </div>
 
                 <div></div>
