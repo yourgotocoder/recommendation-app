2e8ed20b7543c6f32c02ad034732b1a53e5ccfa5
yourgotocoder <yourgotocoder@gmail.com>
2022-07-04 16:19:53 +0530

    Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,13 @@ import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import FooterComponent from "../components/footer/FooterComponent";
 import MainLayout from "../components/layout/MainLayout";
-import { useContext } from "react";
 import { AuthContextProvider } from "../store/AuthContext";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProvider session={session}>
             <Head>
-                <title>
-                    Stock Management App | CSE | Designed By Sudarshan Rai
-                </title>
+                <title>CSE | SMIT</title>
                 <meta charSet="UTF-8" />
             </Head>
             <AuthContextProvider>
